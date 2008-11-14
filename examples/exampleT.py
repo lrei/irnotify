deffa1c073a6af67a0538b867483241c1496da7b
luis.rei <luis.rei@7bb77f8a-b277-11dd-8bee-0be8dff76351>
2008-11-14 18:30:21 +0000

    added the example files and example config files.

---
+++ examples/exampleT.py
@@ -0,0 +1,3 @@
+from twitternotify import TwitterNotify
+n = TwitterNotify("twitter.conf")
+n.notify("lrei", "How do you feel, Rei?")
---
+++ examples/exampleX.py
@@ -0,0 +1,3 @@
+from xmppnotify import XMPPNotify
+n = XMPPNotify("xmpp.conf")
+n.notify("luis.rei@gmail.com", "How do you feel, Rei?")
